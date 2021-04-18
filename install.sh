edbce5e37925756f56f173852fd392b34a713060
OogleGlu <avirukbasak@gmail.com>
2021-04-18 16:00:11 +0530

    Removed useless files

--- install.sh
+++ install.sh
@@ -1,13 +1,7 @@
 echo "Copying files..."
 sudo cp ./gauth /usr/bin
 sudo cp ./ginit /usr/bin
-sudo cp ./gpull /usr/bin
-sudo cp ./gpush /usr/bin
-sudo cp ./gsync /usr/bin
 echo "Setting permission 755..."
 sudo chmod 755 /usr/bin/gauth
 sudo chmod 755 /usr/bin/ginit
-sudo chmod 755 /usr/bin/gpull
-sudo chmod 755 /usr/bin/gpush
-sudo chmod 755 /usr/bin/gsync
 echo "Done"
--- termux-install.sh
+++ termux-install.sh
@@ -2,11 +2,7 @@
 echo "Copying files..."
 cp ./gauth /data/data/com.termux/files/usr/bin
 cp ./ginit /data/data/com.termux/files/usr/bin
-cp ./gpull /data/data/com.termux/files/usr/bin
-cp ./gpush /data/data/com.termux/files/usr/bin
 echo "Setting permission 755..."
 chmod 755 /data/data/com.termux/files/usr/bin/gauth
 chmod 755 /data/data/com.termux/files/usr/bin/ginit
-chmod 755 /data/data/com.termux/files/usr/bin/gpull
-chmod 755 /data/data/com.termux/files/usr/bin/gpush
 echo "Done"
