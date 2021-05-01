9f6cf538047f4ae86fadafeb20cc663b301ee6e3
Oogle Glu <avirukbasak@gmail.com>
2021-05-01 15:03:26 +0530

    update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 echo "Copying files..."
 sudo cp ./gauth /usr/bin
 sudo cp ./ginit /usr/bin
+sudo cp ./gclone /usr/bin
 echo "Setting permission 755..."
 sudo chmod 755 /usr/bin/gauth
 sudo chmod 755 /usr/bin/ginit
+sudo chmod 755 /usr/bin/gclone
 echo "Done"
