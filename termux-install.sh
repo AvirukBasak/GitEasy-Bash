55aafa360b56e3df95962f536b26c85f98fbac6b
Oogle Glu <avirukbasak@gmail.com>
2021-05-01 15:04:11 +0530

    update termux-install.sh

--- termux-install.sh
+++ termux-install.sh
@@ -2,7 +2,9 @@
 echo "Copying files..."
 cp ./gauth /data/data/com.termux/files/usr/bin
 cp ./ginit /data/data/com.termux/files/usr/bin
+cp ./gclone /data/data/com.termux/files/usr/bin/gclone
 echo "Setting permission 755..."
-chmod 755 /data/data/com.termux/files/usr/bin/gauth
-chmod 755 /data/data/com.termux/files/usr/bin/ginit
+chmod 700 /data/data/com.termux/files/usr/bin/gauth
+chmod 700 /data/data/com.termux/files/usr/bin/ginit
+chmod 700 /data/data/com.termux/files/usr/bin/gclone
 echo "Done"
